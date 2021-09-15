3d97a609fda4cd60308cb6268f6e1ba4f8c3db29
Sunki-Kim <83565313+skdding3@users.noreply.github.com>
2021-09-15 23:48:56 +0900

    원하는 값 딕셔너리 도출

---
+++ Algorithm/dict_prac.py
@@ -0,0 +1,20 @@
+# 게임 캐릭터 능력치 저장
+
+sunki = {
+    'health': 575.6,
+    'health_regen': 1.7,
+    'mana': 338.8,
+    'mana_regen': 1.63,
+    'melee': 125,
+    'attack_damage': 60,
+    'attack_speed': 0.625,
+    'armor': 26,
+    'magic_resistance': 32.1,
+    'movement_speed': 340
+}
+
+print(sunki['health'])
+print(sunki['movement_speed'])
+
+
+# health 와 movement_speed 출력
