d411a9362ca12c1e383da6fadeb4b4c33eb663f2
Sunki-Kim <83565313+skdding3@users.noreply.github.com>
2021-09-18 00:35:49 +0900

    두 조건문 (합격조건)

---
+++ Algorithm/if-2.py
@@ -0,0 +1,15 @@
+# 합격 여부 판단하기 else
+
+# 필기 시험 점수가 80점 이상이면 통과라는 조건건
+
+written_test = 75
+coding_test = True
+ 
+if  written_test >= 80 and coding_test == True :
+    print('합격')
+else:
+    print('불합격')
+
+
+
+# if 조건문을 만들고 written_test >= 80 and coding_test == True와 같이 필기 시험 점수가 80점 이상인지 검사하는 조건식과 코딩 시험이 통과했는지 판단하는 조건식을 만듬.
