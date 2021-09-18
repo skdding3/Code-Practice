ac19fa4855cb0f0f7769a29f5ebfbcec6c6ecf4b
Sunki-Kim <83565313+skdding3@users.noreply.github.com>
2021-09-18 19:05:28 +0900

    복합 대입 연산자

---
+++ Algorithm/Assignment Operator.py
@@ -0,0 +1,26 @@
+a = 5
+
+a += 10
+
+a += 20
+
+a += 30
+
+print ("a : ", a)
+
+# 복합 대입 연산자로 += 는 덧셈이후로 대입이 들어간다.  문자열도 가능하다.
+
+string = "스타벅스"
+string += " 프리퀀시"
+
+print("string :",string)
+
+
+
+string2 = input("카페 이름을 입력하세요 >")
+print(string2)
+print(type(string2))
+
+# return : hollys 라 입력할 경우 
+# hollys
+# <class 'str'>
